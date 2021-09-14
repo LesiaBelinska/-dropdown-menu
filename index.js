c5aaa8639eab7e1a4c1de102f1554a585c7c879c
LesiaBelinska <lm.kudak@gmail.com>
2021-09-14 17:56:41 +0300

    change js

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ selected.addEventListener("click", () => {
   optionsContainer.classList.toggle("active");
 });
 
-optionsList.forEach(o => {
-  o.addEventListener("click", () => {
-    selected.innerHTML = o.querySelector("label").innerHTML;
-    optionsContainer.classList.remove("active");
-  });
+optionsContainer.addEventListener("click", (e) => {
+  let selectedCountry = e.target.lastElementChild.textContent;
+  selected.innerHTML = selectedCountry;
+  optionsContainer.classList.remove("active");
+  console.log(selectedCountry);
 });
